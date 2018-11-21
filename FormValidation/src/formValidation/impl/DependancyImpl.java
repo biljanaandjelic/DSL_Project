847dcfe07ac135f8d6eb97f595a5333bf551d7a6
biljanaandjelic <abiljana94@gmail.com>
2018-11-22 00:52:10 +0100

    Create concepts that allow to organize position of UI components.

--- FormValidation/src/formValidation/impl/DependancyImpl.java
+++ FormValidation/src/formValidation/impl/DependancyImpl.java
@@ -1,39 +1,39 @@
-/**
- */
-package formValidation.impl;
-
-import formValidation.Dependancy;
-import formValidation.FormValidationPackage;
-
-import org.eclipse.emf.ecore.EClass;
-
-import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
-
-/**
- * <!-- begin-user-doc -->
- * An implementation of the model object '<em><b>Dependancy</b></em>'.
- * <!-- end-user-doc -->
- *
- * @generated
- */
-public class DependancyImpl extends MinimalEObjectImpl.Container implements Dependancy {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	protected DependancyImpl() {
-		super();
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected EClass eStaticClass() {
-		return FormValidationPackage.Literals.DEPENDANCY;
-	}
-
-} //DependancyImpl
+///**
+// */
+//package formValidation.impl;
+//
+//import formValidation.Dependancy;
+//import formValidation.FormValidationPackage;
+//
+//import org.eclipse.emf.ecore.EClass;
+//
+//import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+//
+///**
+// * <!-- begin-user-doc -->
+// * An implementation of the model object '<em><b>Dependancy</b></em>'.
+// * <!-- end-user-doc -->
+// *
+// * @generated
+// */
+//public class DependancyImpl extends MinimalEObjectImpl.Container implements Dependancy {
+//	/**
+//	 * <!-- begin-user-doc -->
+//	 * <!-- end-user-doc -->
+//	 * @generated
+//	 */
+//	protected DependancyImpl() {
+//		super();
+//	}
+//
+//	/**
+//	 * <!-- begin-user-doc -->
+//	 * <!-- end-user-doc -->
+//	 * @generated
+//	 */
+//	@Override
+//	protected EClass eStaticClass() {
+//		return FormValidationPackage.Literals.DEPENDANCY;
+//	}
+//
+//} //DependancyImpl
