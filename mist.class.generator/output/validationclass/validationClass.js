a6917d62f3929e81ba50e3074c91d9a7cf8e0d8a
Biljana Anđelić <abiljana94@gmail.com>
2019-01-02 19:27:22 +0100

    Generate simple overview for entities.

--- mist.class.generator/output/validationclass/validationClass.js
+++ mist.class.generator/output/validationclass/validationClass.js
@@ -1,65 +1,21 @@
 $(document).ready(function() {
 	
-		$(".dropdown-item").click(function(){
-			var $this = $(this);
-			var panelSelector = $this.attr("href");
-			var $panelToShow = $(panelSelector);
-			
-			$(".panel").hide();
-			$panelToShow.show();
-			
-			
-		});
+$(".dropdown-item").click(function(){
+		var $this = $(this);
+		var panelSelector = $this.attr("href");
+		var $panelToShow = $(panelSelector);
 		
-	$("#submitButton").click(function(e){
-	 if($("form")[0].checkValidity()){
+		$(".panel").hide();
+		$panelToShow.show();
+		
+		
+	});
 
-if($("#adresaElektronskePosteAdministratoraid").val()== $("#adresaElektronskePosteid").val() ||  
- false)
-{
-		e.preventDefault();
-		alert(" Adresa Elektronske Poste and Adresa Elektronske Poste Administratora should be different.");
-}
- if($("#adresaElektronskePosteid").val() != $("#adresaElektronskePosteid").val() || $("#adresaSedistaid").val() != $("#adresaElektronskePosteid").val() )
-{
-	e.preventDefault();
-	alert("Adresa Elektronske Poste  and Adresa Sedista should be same.");
-}
-if($("#nnid").val() > $("#mmid").val())
-{
-	e.preventDefault();
-	alert("Mm  should be greaterthan Nn .");
-}
 
-	}
-});
-});
-
-if($("#adresaElektronskePosteAdministratoraid").val()== $("#adresaElektronskePosteid").val() ||  
- false)
-{
-		e.preventDefault();
-		alert(" Adresa Elektronske Poste and Adresa Elektronske Poste Administratora should be different.");
-}
- if($("#adresaElektronskePosteid").val() != $("#adresaElektronskePosteid").val() || $("#adresaSedistaid").val() != $("#adresaElektronskePosteid").val() )
-{
-	e.preventDefault();
-	alert("Adresa Elektronske Poste  and Adresa Sedista should be same.");
-}
-if($("#nnid").val() > $("#mmid").val())
-{
-	e.preventDefault();
-	alert("Mm  should be greaterthan Nn .");
-}
 $(function() {
-  var availableTags = [
-    "ActionScript", "AppleScript", "Asp", "BASIC", "C", "C++",
-    "Clojure", "COBOL", "ColdFusion", "Erlang", "Fortran",
-    "Groovy", "Haskell", "Java", "JavaScript", "Lisp", "Perl",
-    "PHP", "Python", "Ruby", "Scala", "Scheme"
-  ];
-  
-  $("#brojOrganizacioneJedinice").autocomplete({
-    source: availableTags
-  });
+var brojOrganizacioneJediniceAvailableTags = [];
+$("#brojOrganizacioneJedinice").autocomplete({
+  	source: brojOrganizacioneJediniceAvailableTags
+  	});
+});
 });
